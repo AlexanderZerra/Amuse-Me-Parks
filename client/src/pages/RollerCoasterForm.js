a35a5312973fec5c20fc07b261fe95be9f3a109a
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-06-04 09:25:05 -0700

    more css styling and background

--- client/src/pages/RollerCoasterForm.js
+++ client/src/pages/RollerCoasterForm.js
@@ -32,62 +32,69 @@ export default class RollerCoasterForm extends Component {
   render() {
     console.log(this.state)
     return (
-      <div>
-        <h1>RollercoasterForm</h1>
-        <form onSubmit={(e) => this.handlePush(e)}>
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChange}
-            name="name"
-            placeholder="coaster name"
-          />
-          <input
-            type="text"
-            value={this.state.themePark}
-            onChange={this.handleChange}
-            name="themePark"
-            placeholder="themePark"
-          />
+      <body className="photo">
+        <div>
+          <header>
+            <h1 className="forminfo">RollercoasterForm</h1>
+            <form onSubmit={(e) => this.handlePush(e)}>
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={this.handleChange}
+                name="name"
+                placeholder="coaster name"
+              />
+              <input
+                type="text"
+                value={this.state.themePark}
+                onChange={this.handleChange}
+                name="themePark"
+                placeholder="themePark"
+              />
 
-          <input
-            type="text"
-            value={this.state.description}
-            onChange={this.handleChange}
-            name="description"
-            placeholder="coaster description"
-          />
-          <input
-            type="text"
-            value={this.state.height}
-            onChange={this.handleChange}
-            name="height"
-            placeholder="coaster height"
-          />
-          <input
-            type="text"
-            value={this.state.length}
-            onChange={this.handleChange}
-            name="length"
-            placeholder="coaster length"
-          />
-          <input
-            type="text"
-            value={this.state.topSpeed}
-            onChange={this.handleChange}
-            name="topSpeed"
-            placeholder="coaster topSpeed"
-          />
-          <input
-            type="text"
-            value={this.state.image}
-            onChange={this.handleChange}
-            name="image"
-            placeholder="image url"
-          />
-          <button>Submit</button>
-        </form>
-      </div>
+              <input
+                type="text"
+                value={this.state.description}
+                onChange={this.handleChange}
+                name="description"
+                placeholder="coaster description"
+              />
+              <input
+                type="text"
+                value={this.state.height}
+                onChange={this.handleChange}
+                name="height"
+                placeholder="coaster height"
+              />
+              <input
+                type="text"
+                value={this.state.length}
+                onChange={this.handleChange}
+                name="length"
+                placeholder="coaster length"
+              />
+              <input
+                type="text"
+                value={this.state.topSpeed}
+                onChange={this.handleChange}
+                name="topSpeed"
+                placeholder="coaster topSpeed"
+              />
+              <input
+                type="text"
+                value={this.state.image}
+                onChange={this.handleChange}
+                name="image"
+                placeholder="image url"
+              />
+              <button>Submit</button>
+            </form>
+          </header>
+          <p className="informationtext">
+            Enter in all fields of data before hitting the submit
+          </p>
+        </div>
+      </body>
     )
   }
 }
